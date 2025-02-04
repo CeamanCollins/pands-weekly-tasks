937990a689ff268db6e4c818605b3e5a503c37f3
Ceaman Collins <ceaman.collins@gmail.com>
2025-02-04 18:56:46 +0000

    added references to bank.py

--- bank.py
+++ bank.py
@@ -7,3 +7,5 @@ firstvalue = int(input("Enter amount1(in cent): "))
 secondvalue = int(input("Enter amount1(in cent): "))
 # Adding the two values and printing the result to 2 decimal places.
 print("The sum of these is €{0:.2f}".format((firstvalue + secondvalue)/100))
+
+# References used: Whirlwind Tour of Python, Built-In Types: Simple Values, Aside: Floating-point precision
