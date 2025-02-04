8387277369f6580eff736d5b62097d7d13d5b7c7
Ceaman Collins <ceaman.collins@gmail.com>
2025-02-04 17:41:23 +0000

    added comments to bank.py

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+#Getting user input and assigning it to variables.
 x = int(input("Enter amount1(in cent): "))
 y = int(input("Enter amount1(in cent): "))
+#Adding the two values and printing the result to 2 decimal places.
 print("The sum of these is €{0:.2f}".format((x + y)/100))
