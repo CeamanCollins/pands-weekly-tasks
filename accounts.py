226ef1ea8f5a8f2dd7a245413443d7852cdbc5ad
CeamanCollins <ceaman.collins@gmail.com>
2025-02-12 12:35:43 +0000

    added comments to  accounts.py

--- accounts.py
+++ accounts.py
@@ -6,8 +6,10 @@ account_number = input("Please enter an 10 digit account number: ")
 # https://stackoverflow.com/questions/2030053/how-to-generate-random-strings-in-python
 censored_number = ''
 
+# adding  the Xs
 for i in range(0,len(account_number)-4):
     censored_number += "X"
+# adding thhe last 4 digits
 censored_number += account_number[-4:]
 
 print(censored_number)
