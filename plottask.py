561fac7561148fb977ad5bebbad87814dc17cbe4
CeamanCollins <ceaman.collins@gmail.com>
2025-03-19 00:42:08 +0000

    cleaning up and adding png

--- plottask.py
+++ plottask.py
@@ -2,9 +2,6 @@
 # Author: Céaman Collins
 # A script that generates a plot with a histogram and a line.
 
-# a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
-# and a plot of the function  h(x)=x3 in the range 0 to 10, 
-
 # imports
 
 import numpy as np
@@ -23,34 +20,35 @@ random_numbers = np.random.default_rng().normal(loc=5,scale=2, size=1000)
 
 fig, ax = plt.subplots()
 
-# reference used:
 # https://www.geeksforgeeks.org/plotting-histogram-in-python-using-matplotlib/
 # This resource contained the customisation of histogram colours according to height.
 # I appropriated some code from the 'Customized Histogram with Watermark' section.
 
 # creating histogram and assigning result to variables
 
-n, bins, patches = ax.hist(random_numbers,label='Normal Distribution')
+n, bins, patches = ax.hist(random_numbers,label='Normal Distribution', bins=9)
 
 # setting edge colour to black for each bin
 
 for patch in patches:
-    patch.set_edgecolor('black')
+    patch.set_edgecolor('tab:blue')
 
-# getting ratios for each bin
+# getting fractions of the maximum for each bin
 
 fracs = n/n.max()
 
-# normalising colors around the min and max ratio in fracs
+# normalising the fractions and mapping them 
+# linearly to values from 0 to 1
 
 norm = colors.Normalize(fracs.min(), fracs.max())
 
-# using the ratios to give each bin a colour chosen from
+# using the normalisation to give each bin a colour chosen from
 # a colour map according to its relative height
 # slightly adjusted to use a smaller section of the colour map
+# for paler colours
 
 for frac, patch in zip(fracs, patches):
-    colour = plt.cm.Blues(norm(frac*0.3+0.3))
+    colour = plt.cm.Blues(norm(frac)*0.4+0.3)
     patch.set_facecolor(colour)
 
 # generating numbers for use
@@ -67,7 +65,7 @@ y = x ** 3
 
 ax.plot(x,y,label='$y=x^3$',lw=3,alpha=0.75)
 
-# setting x and y bound for axis
+# setting x bound for axis taking n from histogram plot
 
 ax.set_xbound(0,10)
 ax.set_ybound(0,n.max()+50)
@@ -84,7 +82,7 @@ plt.legend()
 
 # save figure as png file
 
-plt.savefig('plottask.png')
+plt.savefig('./pands-weekly-tasks/plottask.png')
 
 # show figure
 
